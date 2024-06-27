be7f950a07f8644b097a7951042945ddfd1ae898
Sage9705 <edem22kumahor@gmail.com>
2024-06-27 10:53:23 +0100

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { Linkedin, Twitter, Github } from 'react-bootstrap-icons'; 
-// import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
 
 export const Footer = () => {
@@ -8,7 +7,6 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          {/* <MailchimpForm /> */}
           <Col xs={12} sm={6}>
             <img src={logo} alt="Logo" />
           </Col>
@@ -18,9 +16,11 @@ export const Footer = () => {
               <a href="#" className="social-link"><Twitter size={32} /></a>
               <a href="#" className="social-link"><Github size={32} /></a>
             </div>
-            <p className="mt-3 mt-sm-0">Copyright 2024.</p>
+            
           </Col>
+          <p className="mt-3 mt-sm-0">Copyright 2024.</p>
         </Row>
+        
       </Container>
     </footer>
   )
