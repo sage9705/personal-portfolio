ad7b57f818e651839a5377029d7a34c1e8f7067c
Sage9705 <edem22kumahor@gmail.com>
2024-06-27 01:12:32 +0100

    Add banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { NavBar } from './components/navbar';
+import { Banner } from './components/banner';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <Banner />
     </div>
   );
 }
