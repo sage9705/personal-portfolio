e0207349e495c18e642baa855d1d3cc3d9ce88af
Sage9705 <edem22kumahor@gmail.com>
2024-06-27 08:34:52 +0100

    Add footer

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
+
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
